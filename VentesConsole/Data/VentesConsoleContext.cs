c46d807ede583c6dfc11def0d843e3e8d2e926f7
Randy TCHUISSEU <randytchuiss@gmail.com>
2024-03-22 18:51:02 +0100

    Changement du lien url après déploiment de l'API

--- VentesConsole/Data/VentesConsoleContext.cs
+++ VentesConsole/Data/VentesConsoleContext.cs
@@ -17,9 +17,11 @@ namespace VentesConsole.Data
 
         public DbSet<VentesConsole.Models.Manufacturer> Manufacturer { get; set; } = default!;
         
-      
-        
         public DbSet<VentesConsole.Models.ConsoleModel> ConsoleModel { get; set; } = default!;
+        /*
+        protected override void OnConfiguring(DbContextOptionsBuilder options) =>
+            options.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=VentesConsoleContext-c7bfb2d8-52a9-482a-942d-421418a945ba;Trusted_Connection=True;MultipleActiveResultSets=true");
+        */
         public DbSet<VentesConsole.Models.Vente> Vente { get; set; } = default!;
     }
 }
